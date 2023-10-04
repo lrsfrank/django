be6455ea991f3e0d0758d117249ff25d1ceba576
frank <805540531@qq.com>
2023-10-04 21:06:42 +0800

    write login and register

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -184,6 +184,11 @@ class Player extends AcGameObject {
         this.move_length = 0;
         this.stamp_time = 0;
         this.cur_skill = null;
+
+        if (this.is_me) {
+            this.img = new Image();
+            this.img.src = this.playground.root.settings.photo;
+        }
     }
     start(){
         if (this.is_me) {
@@ -312,10 +317,21 @@ class Player extends AcGameObject {
 
     }
     render(){
-        this.ctx.beginPath();
-        this.ctx.arc(this.x,this.y,this.radius,0,Math.PI*2, false);
-        this.ctx.fillStyle = this.color;
-        this.ctx.fill();
+        if (this.is_me) {
+            this.ctx.save();
+            this.ctx.beginPath();
+            this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+            this.ctx.stroke();
+            this.ctx.clip();
+            this.ctx.drawImage(this.img, this.x - this.radius, this.y - this.radius, this.radius * 2, this.radius * 2); 
+            this.ctx.restore();
+        } else {
+
+            this.ctx.beginPath();
+            this.ctx.arc(this.x,this.y,this.radius,0,Math.PI*2, false);
+            this.ctx.fillStyle = this.color;
+            this.ctx.fill();
+        }
     }
     on_destroy() {
         for (let i = 0; i < this.playground.players.length; i ++ ){
@@ -438,16 +454,86 @@ class AcGamePlayground {
 class Settings {
     constructor(root) {
         this.root = root;
+        this.username = "";
+        this.photo = "";
+        this.$settings = $(`
+<div class="ac-game-settings">
+    <div class="ac-game-settings-login">
+        <div class="ac-game-settings-login-title">
+            账号登录
+        </div>
+        <div class="ac-game-settings-login-user">
+            <div class="ac-game-settings-login-user-item">
+                <input type="text" placeholder="用户名：">
+            </div>
+            <div class="ac-game-settings-login-user-item">
+                <input type="password" placeholder="密码：">
+            </div>
+        </div>
+        <div class="ac-game-settings-login-user-submit">
+            <button>登录</button>
+        </div>
+        <div class="ac-game-settings-error-messages">
+            用户信息错误
+        </div>
+        <div class="ac-game-settings-option">
+            注册
+        </div>
+    </div>
+    <div class="ac-game-settings-register">
+        <div class="ac-game-settings-login-title">
+            账号注册
+        </div>
+        <div class="ac-game-settings-register-user">
+             <div class="ac-game-settings-register-user-item">
+                <input type="text" placeholder="用户名：">
+             </div>
+             <div class="ac-game-settings-register-user-item">
+                <input type="password" placeholder="密码：">
+             </div>
+             <div class="ac-game-settings-register-user-item">
+                <input type="password" placeholder="确认密码: ">
+             </div>
+        </div>
+        <div class="ac-game-settings-login-user-submit">
+            <button>注册</button>
+        </div>
+        <div class="ac-game-settings-error-messages">
+            账号已存在
+        </div>
+        <div class="ac-game-settings-option">
+            返回登录
+        </div>
+    </div>
+</div>
+        `);
+        this.$login = this.$settings.find(".ac-game-settings-login");
+        this.$login_username = this.$login.find(".ac-game-settings-login-user > :first-child > input");
+        this.$login_password = this.$login.find(".ac-game-settings-login-user > :nth-child(2) > input");
+        this.$login_submit = this.$login.find(".ac-game-settings-login-user-submit > button");
+        this.$login_error = this.$login.find(".ac-game-settings-error-messages");
+        this.$login_error.hide();
+        this.$login_option = this.$login.find(".ac-game-settings-option");
+
+        this.$login.hide();
+
+        this.$register = this.$settings.find(".ac-game-settings-register");
+        this.$register.hide();
+        this.$register_error = this.$register.find(".ac-game-settings-error-messages");
+        this.$register_error.hide();
+        this.root.$ac_game.append(this.$settings);
         this.start();
     }
     start(){
         this.getinfo();
     }
     register() {
-        
+        this.$login.hide();
+        this.$register.show();
     }
     login() {
-        
+        this.$register.hide();
+        this.$login.show();
     }
     getinfo() {
         let outer = this;
@@ -459,12 +545,23 @@ class Settings {
             success: function(reap) {
                 console.log(reap);
                 if (reap.result === "success") {
+                    outer.username = reap.username;
+                    outer.photo = reap.photo;
+                    outer.hide();
                     outer.root.menu.show();
                 } else {
-                    outer.login();
+                    //outer.login();
+                    outer.register();
                 }
             }
-    })
+        })
+    }
+    
+    hide(){
+        this.$settings.hide();
+    }
+    show() {
+        this.$settings.show();
     }
 }
 export class AcGame {
--- game/views/settings/getinfo.py
+++ game/views/settings/getinfo.py
@@ -1,9 +1,16 @@
 from django.http import JsonResponse
 from game.models.player.player import Player
 def getinfo(request):
-    player = Player.objects.all()[0]
-    return JsonResponse({
-        'result': "success",
-        'username': player.user.username,
-        'photo': player.photo,
-    })
+    user = request.user;
+    if not user.is_authenticated:
+        return JsonResponse({
+            'result': "nologin"
+            }) 
+    else:
+        player = Player.objects.all()[0]
+        return JsonResponse({
+            'result': "success",
+            'username': player.user.username,
+            'photo': player.photo,
+            })
+
